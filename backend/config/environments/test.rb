312fe52f89e80d1d56ee12f06bfff3444297e1c7
Ildar Abdulin <ildar.abdulin@gmail.com>
2025-04-13 19:58:13 -0600

    feat(backend): setup CORS, disable forgery protection, allow access from all hosts

--- backend/config/environments/test.rb
+++ backend/config/environments/test.rb
@@ -50,4 +50,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Add all possible hosts for testing
+  config.hosts.clear # Clear the hosts list to allow all hosts in test environment
 end
